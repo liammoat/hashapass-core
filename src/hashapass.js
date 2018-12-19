24618a2fc31da414861bdd1cb01a0fb38156cc95
Liam Moat <liam.moat@microsoft.com>
2018-12-19 17:35:53 +0000

    refactor(ts): initial port from JS to TS for source

--- src/hashapass.js
+++
@@ -1,6 +0,0 @@
-var sha1 = require('./sha1');
-
-module.exports.generate = function (parameter, master, opts) {
-  opts = opts || {};
-  return sha1.b64_hmac_sha1(master, parameter).substr(0, opts.len || 8);
-};
